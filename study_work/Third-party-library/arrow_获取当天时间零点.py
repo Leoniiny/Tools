9df20395b852b0cdd8a3c86cdd794650eea5b38d
Leon <Leiyangp@163.com>
2023-10-13 18:17:33 +0800

    日期：2023年10月12日 修改目录层次 完成对百度图片的爬取以及存储

--- study_work/Third-party-library/arrow_获取当天时间零点.py
+++ study_work/Third-party-library/arrow_获取当天时间零点.py
@@ -1,37 +1,34 @@
 # !/usr/bin python3                                 
 # encoding: utf-8 -*-
-# @Fuction：获取当天时间的0点
-import time
-from datetime import datetime
-
+# @Fuction：获取时间
 import arrow
 
-utc = arrow.utcnow()  # 获取现在的utc时间对象
-test_yesterday = utc.shift(days=-1).format("YYYY-MM-DD HH")
-print(f"test_yesterday  的值为：{test_yesterday}")
 
-local_obj = utc.to('local')  # 将utc时间转换为本地时间
-local_zero = local_obj.replace(hour=0, minute=0, second=0)
-local_time = local_zero.format("YYYY-MM-DD HH:mm:ss")
-today_date = local_zero.format("YYYY-MM-DD")
-yesterday = local_zero.shift(days=-1).format("YYYY-MM-DD")
-print(f"yesterday  的值为：{yesterday}")
+class GetDate():
 
-tendays_after = local_zero.shift(days=10).format("YYYY-MM-DD HH:mm")
-print(f"tendays_after  的值为：{tendays_after}")
+    def __init__(self):
+        self.utc = arrow.utcnow()  # 获取现在的utc时间对象
+
+    def get_date_format(self, format_num =1, formate ="YYYY-MM-DD",interval_format = None,interval_num = 1):
+        if format_num == 2:
+            formate = "YYYY-MM-DD HH"
+        if format_num == 3:
+            formate = "YYYY-MM-DD HH:mm"
+        if format_num == 4:
+            formate = "YYYY-MM-DD HH:mm:ss"
+        if interval_format == "weeks":
+            show_time = self.utc.shift(weeks = interval_num).format(f"{formate}")
+        elif interval_format == "years":
+            show_time = self.utc.shift(years = interval_num).format(f"{formate}")
+        elif interval_format == "months":
+            show_time = self.utc.shift(months = interval_num).format(f"{formate}")
+        else:
+            show_time = self.utc.shift(days = interval_num).format(f"{formate}")
+        print(f"get_date 的值为：{show_time}")
+        return show_time
 
 
+if __name__ == '__main__':
+    obj = GetDate()
+    get_date = obj.get_date_format(format_num= 4)
 
-
-
-print(local_time)  # 输出当天零点时间
-print(today_date)  # 输出当天日期
-print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-today_date01 = datetime.now().strftime('%Y-%m-%d')
-today_date02 = datetime.now().strftime('%Y-%m-%d %H')
-print(today_date01)
-print(f"today_date02  的值为：{today_date02}")
-print(f"time.time()  的值为：{time.time()}")
-startDate = None
-if startDate is None:
-    print(f"startDate  为空，走这里")
